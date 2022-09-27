b3c9dc088a6d99a7d50afab6bac1599296ade2e8
joseluiscrr <joseluiscriollojlcr@gmail.com>
2022-09-27 13:16:38 -0500

    fix: typeof and style hover buttons

--- react/components/ButtonGroup.tsx
+++ react/components/ButtonGroup.tsx
@@ -5,7 +5,7 @@ const ButtonGroup = () => {
   return (
     <div className={`${styles.button__container}`}>
       <a href="/checkout" className={`${styles.checkout}`}>Comprar ahora</a>
-      <a href="/" className={`${styles.continue}`}>Contuniar comprando</a>
+      <a href="/" className={`${styles.continue}`}>Continuar comprando</a>
       <a href="/cart" className={`${styles.cart}`}>Ver carrito</a>
     </div>
   );
